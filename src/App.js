1570af14316f87a343db289060a1dbe81c01738d
@farissahman1 <farissahman1@gmail.com>
2021-11-06 19:28:22 +0100

    auth state changes, authIsReady

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
+import { useAuthContext } from "./hooks/useAuthContext.js";
 import Home from "./pages/home/Home.jsx";
 import Login from "./pages/login/Login.jsx";
 import Signup from "./pages/signup/Signup.jsx";
 
 function App() {
+  const { authIsReady } = useAuthContext();
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="signup" element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+          </Routes>
+        </BrowserRouter>
+      )}
+    </div>
   );
 }
 
---
+++ src/hooks/useLogin.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { auth } from "../firebase/config";
+import { useAuthContext } from "./useAuthContext";
+
+export const useLogin = () => {
+  const [isCanceled, setIsCanceled] = useState(false);
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
+
+  const login = async (email, password) => {
+    setError(null);
+    setIsPending(true);
+
+    try {
+      // sign the user out
+      const res = await auth.signInWithEmailAndPassword(email, password);
+
+      // dispatch logout action
+      dispatch({ type: "LOGIN", payload: res.user });
+
+      // update state
+      if (!isCanceled) {
+        setIsPending(false);
+        setError(null);
+      }
+    } catch (err) {
+      if (!isCanceled) {
+        console.log(err.message);
+        setError(err.message);
+        setIsPending(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    return () => setIsCanceled(true);
+  }, []);
+
+  return { login, error, isPending };
+};
