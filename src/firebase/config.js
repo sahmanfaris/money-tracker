12136a48b948e6ce11944f228880b64f8b418c9d
sahmanfaris <farissahman1@gmail.com>
2021-11-08 11:23:47 +0100

    add & delete docs with custom hooks

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,21 +1,24 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCiRccMqPHShRiz-A1ecRskIW6rB0BJflM",
-  authDomain: "money-tracker-ec217.firebaseapp.com",
-  projectId: "money-tracker-ec217",
-  storageBucket: "money-tracker-ec217.appspot.com",
-  messagingSenderId: "547362172521",
-  appId: "1:547362172521:web:12d1861674ea38e8bffcf8",
-};
+  apiKey: 'AIzaSyCiRccMqPHShRiz-A1ecRskIW6rB0BJflM',
+  authDomain: 'money-tracker-ec217.firebaseapp.com',
+  projectId: 'money-tracker-ec217',
+  storageBucket: 'money-tracker-ec217.appspot.com',
+  messagingSenderId: '547362172521',
+  appId: '1:547362172521:web:12d1861674ea38e8bffcf8',
+}
 
 // init firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 
 // init services
-const db = firebase.firestore();
-const auth = firebase.auth();
+const db = firebase.firestore()
+const auth = firebase.auth()
 
-export { db, auth };
+// timestamp
+const timestamp = firebase.firestore.Timestamp
+
+export { db, auth, timestamp }
